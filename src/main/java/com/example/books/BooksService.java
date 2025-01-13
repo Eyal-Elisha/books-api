731f400d5a12916a67275e7618ff97788c50c93a
NoamElisha <noampro21@gmail.com>
2025-01-13 19:43:00 +0200

    working on skeleton

--- src/main/java/com/example/books/BooksService.java
+++ src/main/java/com/example/books/BooksService.java
@@ -1,4 +1,36 @@
 package com.example.books;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BooksService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BooksService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return null;
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return null;
+    }
+
+    public Book addBook(Book book) {
+        return null;
+    }
+
+    public Optional<Book> updateBook(Long id, Book updatedBook) {
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+    }
 }
